ec9fa61ba2f1227fc9de92962ad1ca5bf597ddf3
claudio gomez <clauivangomez48@gmail.com>
2025-12-11 02:00:35 -0300

    Primer commit: Subiendo proyecto SpringBoot

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,48 @@
+package com.example.demo.model;
+
+
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+    private int id;
+
+    public Product(String name, double price, String description, int id) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
